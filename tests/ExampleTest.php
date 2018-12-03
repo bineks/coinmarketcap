901686fdf6a941a0e1b6266fbae2c500c5d0b935
Anatoliy <lightduelist@gmail.com>
2018-12-03 16:52:28 +0200

    add test example

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class ExampleTest
+ */
+class ExampleTest extends Orchestra\Testbench\TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
